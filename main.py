46e5be392dfcccc293f8590f9ec6717a61238b1a
Achyut Neupane <achyutkneupane@gmail.com>
2024-01-04 22:23:18 +0545

    Task 1 : Plotting Towns in England and Wales on UK Map

--- main.py
+++ main.py
@@ -22,10 +22,38 @@ class LatLon:
         return f"{self.town_name} : [{self.latitude}, {self.longitude}]"
 
 
-class User:
+def plot_specific_towns():
+    # Read in the towns data (from latlon.csv)
+    towns = []
+    with open(lat_lon_file, "r") as f:
+        for line in f:
+            town_name, latitude, longitude = line.strip().split(",")
+            towns.append(LatLon(town_name, float(latitude), float(longitude)))
 
-
+    # Initialise the UKMap object
+    uk_map = UKMap()
+    for town in towns:
+        """
+        Plotting the towns to be easily found on the map.
+        Marker used: o
+        Marker size: 6
+        Color: green
+        
+        
+        Plotting the towns that fulfil any of the following criteria:
+        • The town name starts with A, B, C, L or M.
+        • The town name ends with “bury” or “ampton”.
+        
+        Marker used: .
+        Marker size: 1
+        Color: red
+        """
+        if town.town_name in ["Aberystwyth", "Birmingham", "Cardiff", "London"]:
+            uk_map.plot(town.longitude, town.latitude, marker="o", markersize=6, color="green")
+        elif town.town_name[0] in ["A", "B", "C", "L", "M"] or town.town_name[-4:] in ["bury", "ampton"]:
+            uk_map.plot(town.longitude, town.latitude, markersize=1, color="red")
+    uk_map.show()
 
 
 if __name__ == "__main__":
-    main()
+    plot_specific_towns()
